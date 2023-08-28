0177d22037178428d2d1946a72361f5adcb9ff4e
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-08-27 20:54:36 -0400

    modelos listos

---
+++ app/models/index.js
@@ -0,0 +1,33 @@
+const dbConfig = require("../config/db.config");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(dbConfig.DB,dbConfig.USER,dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorAliases: false,
+    pool: {
+        max: dbConfig.max,
+        min: dbConfig.min,
+        acquire: dbConfig.acquire,
+        idle: dbConfig.idle
+    }
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.users = require("./user.model")(sequelize,Sequelize);
+db.projects = require("./project.model")(sequelize,Sequelize);
+
+db.users.hasMany(db.projects, {
+    as: 'projects' //Con este nick sale en la consulta
+});
+
+db.projects.belongsTo(db.users, {
+    foreignKey: 'userId',
+    as: 'user'
+});
+
+module.exports = db;
